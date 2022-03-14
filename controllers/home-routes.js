dc0d42c250e2f7e705a275353c1c1cad8f8df218
themadtaco <aatrox48@gmail.com>
2022-03-13 21:41:34 -0500

    added handlebar templates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,39 @@ const { Post, User, Comment } = require('../models');
 
 // render homepage with posts
 router.get('/', (req, res) => {
-    res.render('homepage');
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
